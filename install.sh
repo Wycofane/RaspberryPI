83cff0f09161b27a3bc801c2f020e117c62cc4df
Wycofane <it9-vohju@student.itech-bs14.de>
2021-08-20 15:06:17 +0200

    V2.1

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 sudo apt update -y && sudo apt upgrade -y
 apt install python3-pip -y
-pip3 install psutil
 pip3 install flask
-pip3 install flask-mobility
 pip3 install flask-limiter
 pip3 install mysql-connector-python
